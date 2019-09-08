19cfbf3b094da86c128e556c9fe833c73e7822c6
Wayne Gong <nju_gw@msn.com>
2019-09-08 10:53:56 +0800

    initial commit

---
+++ account-svc/src/main/java/io/wayne/store/accountsvc/controller/AccountController.java
@@ -0,0 +1,17 @@
+package io.wayne.store.accountsvc.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/account")
+@Slf4j
+public class AccountController {
+
+    @GetMapping("ping")
+    public String ping(){
+        return "pong";
+    }
+}
---
+++ store-common-lib/src/main/java/io/wayne/storecommonlib/StoreCommonLibApplication.java
@@ -0,0 +1,13 @@
+package io.wayne.storecommonlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreCommonLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreCommonLibApplication.class, args);
+    }
+
+}
